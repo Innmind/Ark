eaa6664b31f3bc47f03dabb8990b0bb9526418f6
Baptiste Langlade <baptouuuu@gmail.com>
2018-03-21 08:23:07 +0100

    add strategies to know if a server is available to be used as a new installation

---
+++ src/Forge/OVH/Available/Consensus.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Ark\Forge\OVH\Available;
+
+use Innmind\Ark\{
+    Forge\OVH\Available,
+    Installation\Name,
+};
+use Innmind\Immutable\Sequence;
+
+final class Consensus implements Available
+{
+    private $strategies;
+
+    public function __construct(Available ...$strategies)
+    {
+        $this->strategies = Sequence::of(...$strategies);
+    }
+
+    public function __invoke(Name $name): bool
+    {
+        return $this->strategies->reduce(
+            true,
+            static function(bool $available, Available $strategy) use ($name): bool {
+                return $available && $strategy($name);
+            }
+        );
+    }
+}
---
+++ src/Forge/OVH/Available/State.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Ark\Forge\OVH\Available;
+
+use Innmind\Ark\{
+    Forge\OVH\Available,
+    Installation\Name,
+};
+use Ovh\Api;
+
+final class State implements Available
+{
+    private $api;
+
+    public function __construct(Api $api)
+    {
+        $this->api = $api;
+    }
+
+    public function __invoke(Name $name): bool
+    {
+        try {
+            return $this->api->get('/vps/'.$name)['state'] === 'stopped';
+        } catch (\Throwable $e) {
+            return false;
+        }
+    }
+}
