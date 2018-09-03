23544a03065e3acb2ad5533f8a9c982aef100909
Baptiste Langlade <baptouuuu@gmail.com>
2018-09-03 20:49:23 +0200

    add bootstrap function as an alternative to service container

---
+++ src/bootstrap.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Ark;
+
+use Innmind\Url\PathInterface;
+use Innmind\Server\Control\ServerFactory;
+use Ovh\Api;
+
+function bootstrap(
+    Api $api,
+    PathInterface $sshFolder,
+    Server $server = null
+): Ark {
+    $server = $server ?? ServerFactory::build();
+
+    return new Ark(
+        new Forge\Ovh(
+            $api,
+            $available = new Forge\Ovh\Available\State($api),
+            new Forge\Ovh\Bootstrap\Reinstall($api, $server, $sshFolder),
+            new Forge\Ovh\Dispose\Stop($api)
+        ),
+        new InstallationArray\Ovh($api, $available)
+    );
+}
